dfc738ceb8efd6c8971f3ed551f761942ff57967
seeeheng <siheng.teng@gmail.com>
2020-04-29 17:02:21 +0800

    beacons added in

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var map = L.map('map', {
     minZoom: -5
 });
 
-var bounds = [[0,0], [1600,739]];
+var bounds = [[0,0], [728,454]];
 var image = L.imageOverlay('room.jpg', bounds).addTo(map);
+var beacon_1 = L.latLng([710,182]);
+var beacon_2 = L.latLng([710,434]);
+L.marker(beacon_1).addTo(map);
+L.marker(beacon_2).addTo(map);
+
 map.fitBounds(bounds);

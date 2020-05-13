e6952a9f7e0b399e7e424718e8870fdd0d7b1e12
seeeheng <siheng.teng@gmail.com>
2020-05-13 20:26:35 +0800

    included deployment @ /build

---
+++ 2_react-leaflet-frontend/build/precache-manifest.dd742a9eedf052cbd1dc16c2d106f254.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23062855f3c174e4716c02e82c4ac9da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2120ab986aa20420b10e",
+    "url": "/static/css/2.6f3a26c5.chunk.css"
+  },
+  {
+    "revision": "5765ae301046a05adeb4",
+    "url": "/static/css/main.5a0e6d56.chunk.css"
+  },
+  {
+    "revision": "2120ab986aa20420b10e",
+    "url": "/static/js/2.252494de.chunk.js"
+  },
+  {
+    "revision": "b0eae4bb21377c1a3651bcefa2010b3f",
+    "url": "/static/js/2.252494de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5765ae301046a05adeb4",
+    "url": "/static/js/main.38ba5906.chunk.js"
+  },
+  {
+    "revision": "48dd1821eb17d2cea1a0",
+    "url": "/static/js/runtime-main.40d19f1e.js"
+  },
+  {
+    "revision": "df66aa78da86056fead880abcfb8c7fb",
+    "url": "/static/media/dicpart1.df66aa78.jpg"
+  },
+  {
+    "revision": "8d8efb3f1d09e8114cd1bb18ea430fa9",
+    "url": "/static/media/livingroom.8d8efb3f.png"
+  },
+  {
+    "revision": "e678e33731d89be8feac521eae1bfb8e",
+    "url": "/static/media/room.e678e337.png"
+  }
+]);
